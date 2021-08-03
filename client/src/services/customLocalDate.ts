ad85498c5352286bc676a5ba4ec7e0c81fe2266b
NLTTSKY <t.tian981225@gmail.com>
2021-08-03 08:40:41 +0000

    adding custom function stringToDate

--- client/src/services/customLocalDate.ts
+++ client/src/services/customLocalDate.ts
@@ -7,3 +7,13 @@ export function customLocalDate(date: Date) {
 export function customLocalTime(date: Date) {
   return `${date.getHours()}:${('0' + date.getMinutes()).slice(-2)}`;
 }
+
+// Function to check if date is within the last two weeks - used in reportDetails - returns boolean
+export function customStringToDate(date: String) {
+ let dateofsurvey= date.split('T')[0]
+ let firstDate = Date.parse(dateofsurvey)
+ const oneDay = 24 * 60 * 60 * 1000; 
+
+ const diffDays = Math.round(Math.abs((firstDate - new Date().getTime()) / oneDay));
+ return diffDays<15;
+}
